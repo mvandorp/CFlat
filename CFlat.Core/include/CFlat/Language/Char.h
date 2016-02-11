682fcc796350915450047ddf946def27f5fb2a97
Martijn van Dorp <martijnvandorp@casema.nl>
2016-02-11 01:52:11 +0100

    Add character testing functions

---
+++ CFlat.Core/include/CFlat/Language/Char.h
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2015 Martijn van Dorp
+ *
+ * This file is part of CFlat.Core.
+ *
+ * CFlat.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CFlat.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file Char.h
+ */
+
+#ifndef CFLAT_CORE_LANGUAGE_CHAR_H
+#define CFLAT_CORE_LANGUAGE_CHAR_H
+
+#include "CFlat/Language/Boolean.h"
+
+/// <summary>
+/// Returns whether the given character is catagorized as a binary digit.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a binary digit; otherwise <see cref="false"/>.</returns>
+bool char_IsBinaryDigit(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a control character.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a control character; otherwise <see cref="false"/>.</returns>
+bool char_IsControl(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a decimal digit.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a decimal digit; otherwise <see cref="false"/>.</returns>
+bool char_IsDigit(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a graphical character.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a graphical character; otherwise <see cref="false"/>.</returns>
+bool char_IsGraphic(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a hexadecimal digit.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a hexadecimal digit; otherwise <see cref="false"/>.</returns>
+bool char_IsHexadecimalDigit(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a letter.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a letter; otherwise <see cref="false"/>.</returns>
+bool char_IsLetter(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a letter or decimal digit.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a letter or decimal digit; otherwise <see cref="false"/>.</returns>
+bool char_IsLetterOrDigit(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a lowercase letter.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a lowercase letter; otherwise <see cref="false"/>.</returns>
+bool char_IsLower(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a printable character.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a printable character; otherwise <see cref="false"/>.</returns>
+bool char_IsPrintable(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as punctation.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is punctation; otherwise <see cref="false"/>.</returns>
+bool char_IsPunctation(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as a separator character.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is a separator character; otherwise <see cref="false"/>.</returns>
+bool char_IsSeparator(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as an uppercase letter.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is an uppercase letter; otherwise <see cref="false"/>.</returns>
+bool char_IsUpper(int c);
+
+/// <summary>
+/// Returns whether the given character is catagorized as white space.
+/// </summary>
+/// <param name="c">The character to check.</param>
+/// <returns><see cref="true"/> if the character is white space; otherwise <see cref="false"/>.</returns>
+bool char_IsWhiteSpace(int c);
+
+/// <summary>
+/// Converts the given character to its lowercase equivalent.
+/// </summary>
+/// <param name="c">The character to convert.</param>
+/// <returns>
+/// The lowercase equivalent of <c>c</c> if <c>c</c> is an uppercase letter; otherwise returns <c>c</c>.
+/// </returns>
+int char_ToLower(int c);
+
+/// <summary>
+/// Converts the given character to its upercase equivalent.
+/// </summary>
+/// <param name="c">The character to convert.</param>
+/// <returns>
+/// The uppercase equivalent of <c>c</c> if <c>c</c> is a lowercase letter; otherwise returns <c>c</c>.
+/// </returns>
+int char_ToUpper(int c);
+
+#endif
---
+++ CFlat.Core/src/CFlat/Language/Char.c
@@ -0,0 +1,266 @@
+#include "CFlat/Language/Char.h"
+
+#include "CFlat.h"
+
+#define CHAR_CONTROL            0x0001
+#define CHAR_LOWER              0x0002
+#define CHAR_UPPER              0x0004
+#define CHAR_DIGIT              0x0008
+#define CHAR_BINARY             0x0010
+#define CHAR_HEX                0x0020
+#define CHAR_PUNCTATION         0x0040
+#define CHAR_WHITESPACE         0x0080
+#define CHAR_SEPARATOR          0x0100
+#define CHAR_LETTER             (CHAR_LOWER | CHAR_UPPER)
+#define CHAR_LETTER_OR_DIGIT    (CHAR_LETTER | CHAR_DIGIT)
+#define CHAR_GRAPHIC            (CHAR_LETTER_OR_DIGIT | CHAR_PUNCTATION)
+#define CHAR_PRINTABLE          (CHAR_GRAPHIC | CHAR_SEPARATOR)
+
+#define ASCII_MASK              0x7F
+#define IS_ASCII(c)             !((c) & ~ASCII_MASK)
+#define CHAR_IS(c, group)       ((LookupTable[(c) & ASCII_MASK] & (group)) && IS_ASCII(c))
+
+// Maps characters to character groups.
+static const int LookupTable[128] =
+{
+    /* 0x00 .. 0x08 */
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+
+    /* 0x09 .. 0x0D */
+    CHAR_CONTROL | CHAR_WHITESPACE,
+    CHAR_CONTROL | CHAR_WHITESPACE,
+    CHAR_CONTROL | CHAR_WHITESPACE,
+    CHAR_CONTROL | CHAR_WHITESPACE,
+    CHAR_CONTROL | CHAR_WHITESPACE,
+
+    /* 0x0E .. 0x1F */
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+    CHAR_CONTROL,
+
+    /* 0x20 */
+    CHAR_WHITESPACE | CHAR_SEPARATOR,
+
+    /* 0x21 .. 0x2F */
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+
+    /* 0x30 .. 0x39 */
+    CHAR_DIGIT | CHAR_HEX | CHAR_BINARY,
+    CHAR_DIGIT | CHAR_HEX | CHAR_BINARY,
+    CHAR_DIGIT | CHAR_HEX,
+    CHAR_DIGIT | CHAR_HEX,
+    CHAR_DIGIT | CHAR_HEX,
+    CHAR_DIGIT | CHAR_HEX,
+    CHAR_DIGIT | CHAR_HEX,
+    CHAR_DIGIT | CHAR_HEX,
+    CHAR_DIGIT | CHAR_HEX,
+    CHAR_DIGIT | CHAR_HEX,
+
+    /* 0x3A .. 0x40 */
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+
+    /* 0x41 .. 0x46 */
+    CHAR_UPPER | CHAR_HEX,
+    CHAR_UPPER | CHAR_HEX,
+    CHAR_UPPER | CHAR_HEX,
+    CHAR_UPPER | CHAR_HEX,
+    CHAR_UPPER | CHAR_HEX,
+    CHAR_UPPER | CHAR_HEX,
+
+    /* 0x47 .. 0x5A */
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+    CHAR_UPPER,
+
+    /* 0x5B .. 0x60 */
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+
+    /* 0x61 .. 0x66 */
+    CHAR_LOWER | CHAR_HEX,
+    CHAR_LOWER | CHAR_HEX,
+    CHAR_LOWER | CHAR_HEX,
+    CHAR_LOWER | CHAR_HEX,
+    CHAR_LOWER | CHAR_HEX,
+    CHAR_LOWER | CHAR_HEX,
+
+    /* 0x67 .. 0x7A */
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+    CHAR_LOWER,
+
+    /* 0x7B .. 0x7E */
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+    CHAR_PUNCTATION,
+
+    /* 0x7F */
+    CHAR_CONTROL,
+};
+
+bool char_IsBinaryDigit(int c)
+{
+    return CHAR_IS(c, CHAR_BINARY);
+}
+
+bool char_IsControl(int c)
+{
+    return CHAR_IS(c, CHAR_CONTROL);
+}
+
+bool char_IsDigit(int c)
+{
+    return CHAR_IS(c, CHAR_DIGIT);
+}
+
+bool char_IsGraphic(int c)
+{
+    return CHAR_IS(c, CHAR_GRAPHIC);
+}
+
+bool char_IsHexDigit(int c)
+{
+    return CHAR_IS(c, CHAR_HEX);
+}
+
+bool char_IsLetter(int c)
+{
+    return CHAR_IS(c, CHAR_LETTER);
+}
+
+bool char_IsLetterOrDigit(int c)
+{
+    return CHAR_IS(c, CHAR_LETTER_OR_DIGIT);
+}
+
+bool char_IsLower(int c)
+{
+    return CHAR_IS(c, CHAR_LOWER);
+}
+
+bool char_IsPrintable(int c)
+{
+    return CHAR_IS(c, CHAR_PRINTABLE);
+}
+
+bool char_IsPunctation(int c)
+{
+    return CHAR_IS(c, CHAR_PUNCTATION);
+}
+
+bool char_IsSeparator(int c)
+{
+    return CHAR_IS(c, CHAR_SEPARATOR);
+}
+
+bool char_IsUpper(int c)
+{
+    return CHAR_IS(c, CHAR_UPPER);
+}
+
+bool char_IsWhiteSpace(int c)
+{
+    return CHAR_IS(c, CHAR_WHITESPACE);
+}
+
+int char_ToLower(int c)
+{
+    if (char_IsUpper(c)) {
+        return 'a' + c - 'A';
+    }
+    else {
+        return c;
+    }
+}
+
+int char_ToUpper(int c)
+{
+    if (char_IsLower(c)) {
+        return 'A' + c - 'a';
+    }
+    else {
+        return c;
+    }
+}
