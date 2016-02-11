29e0b5fdc0b5f165128e92f3817ccdcfc6959a1c
Martijn van Dorp <martijnvandorp@casema.nl>
2016-02-11 01:51:17 +0100

    Add null macro
    Added a macro that expands to a null pointer constant

---
+++ CFlat.Core/include/CFlat/Language/Null.h
@@ -0,0 +1,13 @@
+/// @file Null.h
+/// @author Martijn van Dorp
+/// @date July 2015
+
+#ifndef CFLAT_CORE_LANGUAGE_NULL_H
+#define CFLAT_CORE_LANGUAGE_NULL_H
+
+/// <summary>
+/// The null pointer constant.
+/// </summary>
+#define null ((void*)0)
+
+#endif
