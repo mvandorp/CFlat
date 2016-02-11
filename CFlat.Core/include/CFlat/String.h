ba52dcdfa51c8b81b740f278dca1f1c7dc6bcdd2
Martijn van Dorp <martijnvandorp@casema.nl>
2016-02-11 01:51:45 +0100

    Add licenses

--- CFlat.Core/include/CFlat/String.h
+++ CFlat.Core/include/CFlat/String.h
@@ -1,11 +1,30 @@
-/// @file String.h
-/// @author Martijn van Dorp
-/// @date August 2015
+/*
+ * Copyright (C) 2015 Martijn van Dorp
+ *
+ * This file is part of CFlat.Core.
+ *
+ * CFlat.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CFlat.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @file String.h
+ */
 
 #ifndef CFLAT_CORE_STRING_H
 #define CFLAT_CORE_STRING_H
 
-#include "CFlat.h"
+#include "CFlat/Language/Integer.h"
 
 /// <summary>
 /// Represents text as an immutable string of characters.
--- CFlat.Core/src/CFlat/String-private.h
+++ CFlat.Core/src/CFlat/String-private.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 Martijn van Dorp
+ *
+ * This file is part of CFlat.Core.
+ *
+ * CFlat.Core is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CFlat.Core is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef CFLAT_CORE_STRING_PRIVATE_H
 #define CFLAT_CORE_STRING_PRIVATE_H
 
@@ -15,10 +34,10 @@
 /// <param name="value">A string literal.</param>
 #define CFLAT_STRING_LITERAL(value) { CFLAT_OBJECT_INITIALIZER(null), CFLAT_STRING_LITERAL_LENGTH(value), value }
 
-struct String {
+typedef struct String {
     Object object;
     uintsize length;
     const char* value;
-};
+} String;
 
 #endif
