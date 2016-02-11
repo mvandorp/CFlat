f10bc3e94f2336369e063458134c11c3fc0f72be
Martijn van Dorp <martijnvandorp@casema.nl>
2016-02-11 01:51:12 +0100

    Add boolean type
    Added a typedef 'bool' for a boolean type.
    
    Added two bool constants:
     - 'true' which has the integer value 1
     - 'false' which has the integer value 0

---
+++ CFlat.Core/include/CFlat/Language/Boolean.h
@@ -0,0 +1,36 @@
+/// @file Boolean.h
+/// @author Martijn van Dorp
+/// @date July 2015
+
+#ifndef CFLAT_CORE_LANGUAGE_BOOLEAN_H
+#define CFLAT_CORE_LANGUAGE_BOOLEAN_H
+
+#if (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L) || (defined(_MSC_VER) && (_MSC_VER >= 1800))
+    /* The compiler supports C99 or later, use the C99 _Bool type */
+    #define CFLAT_BOOL _Bool
+#elif defined(_MSC_VER)
+    /* Visual Studio 2012 or earlier, use the int type */
+    #define CFLAT_BOOL int
+#else
+    /* Otherwise use the char type */
+    #define CFLAT_BOOL char
+#endif
+
+/// <summary>
+/// Represents a boolean value, which can be either <see cref="true"/> or <see cref="false"/>.
+/// </summary>
+typedef CFLAT_BOOL bool;
+
+/// <summary>
+/// The <see cref="bool"/> value <c>true</c>.
+/// </summary>
+const bool true = 1;
+
+/// <summary>
+/// The <see cref="bool"/> value <c>false</c>.
+/// </summary>
+const bool false = 0;
+
+#undef CFLAT_BOOL
+
+#endif
