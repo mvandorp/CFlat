3cd9e4022a809d6b2f670368163aa976aca55be1
Martijn van Dorp <martijnvandorp@casema.nl>
2016-02-11 01:51:19 +0100

    Add CFlat.h header
    Added header file CFlat.h which shall include header files that are
    commonly needed in most files.

---
+++ CFlat.Core/include/CFlat.h
@@ -0,0 +1,14 @@
+/// @file CFlat.h
+/// @author Martijn van Dorp
+/// @date July 2015
+
+#ifndef CFLAT_CORE_CFLAT_H
+#define CFLAT_CORE_CFLAT_H
+
+#include <assert.h>
+
+#include "CFlat/Language/Boolean.h"
+#include "CFlat/Language/Integer.h"
+#include "CFlat/Language/Null.h"
+
+#endif
